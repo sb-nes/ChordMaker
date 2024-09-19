0d7684e91495c05adc5712c6032f467cdec37a46
shrey <shrey@DESKTOP-GVNAU3D>
2024-09-19 18:54:48 +0530

    Add project files.

---
+++ Chord Maker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Chord_Maker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
