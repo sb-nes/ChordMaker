1a4cd0fb42f704ebd87eceec6fc5c9fa2ffed1f2
sb-nes <shreyashbara2001@outlook.com>
2024-09-19 19:53:22 +0530

    Basic Idea

---
+++ Chord Maker/Common/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Chord_Maker
+{
+    public class ViewModelBase : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
